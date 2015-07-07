949a1778a4efa32dd868e02d916ca4998739b139
Nic Grayson <nic.grayson@banno.com>
2015-07-07 16:02:35 -0500

    fixed cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-curl http://192.168.33.10:7000/api/brokers/stop?id=0
-curl http://192.168.33.10:7000/api/brokers/stop?id=1
-curl http://192.168.33.10:7000/api/brokers/stop?id=2
-
-curl http://192.168.33.10:7000/api/brokers/remove?id=0
-curl http://192.168.33.10:7000/api/brokers/remove?id=1
-curl http://192.168.33.10:7000/api/brokers/remove?id=2
-
+for x in 0 1 2 3 4 5;
+do
+    curl http://192.168.33.10:7000/api/brokers/stop?id=$x
+    curl http://192.168.33.10:7000/api/brokers/remove?id=$x
+done
 
 curl http://192.168.33.10:7000/api/brokers/status | jq .
